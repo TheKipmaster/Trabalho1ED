62fa1b38f074829d82c17b5b04448229ca50bbd0
Kipman <felippekipman@cjr.org.br>
2016-11-05 18:25:35 -0200

    -Terceira versao dos comentarios doxygen

--- funcoes.h
+++ funcoes.h
@@ -8,58 +8,20 @@ typedef struct pilha {
 } t_pilha;
 
 /**
-* Desenha o menu e valida a entrada do usuário quanto ao seus ítens.
-*/
+ * Desenha o menu e valida a entrada do usuário quanto ao seus ítens.
+ */
 int 	 drawMenu (int *n);
-
 /**
-* Usa as funções de manipulação de pilhas e as presentes 
-* no arquivo 'expressao.c' para validar, transformar para
-* forma posfixa e então resolver uma expressão dada.
-*/
+ * Usa as funções de manipulação de pilhas e as presentes 
+ * no arquivo 'expressao.c' para validar, transformar para
+ * forma posfixa e então resolver uma expressão dada.
+ */
 void 	 funcao1  (int *n);
-
 /**
-* Usa as funções de manipulação de pilhas e as presentes no arquivo 
-* 'expressao.c' para criar a calculadora descrita pelo trabalho.
-*/
+ * Usa as funções de manipulação de pilhas e as presentes no arquivo 
+ * 'expressao.c' para criar a calculadora descrita pelo trabalho.
+ */
 void 	 funcao2  (int *n);
-
-/**
-* Recebe um caractere de fechamento de escopo e 
-* retira elementos da pilha até encontrá-lo.
-*/
-int		 desemPilha   (t_pilha *pilha, char parentese, char saida[], int *j);
-
-/**
-* Recebe uma expressão no formato string e valida ela quanto aos delimitadores
-* de escopo. Retorna '1' caso seja válida e '0' caso contrário.
-*/
-int		 validaExpressao  (char expressao[], int *tamanho);
-
-/**
-* Após validada, converte a espressão para a notação posfixa, adicionando
-* espaços para facilitar a resolução dela pela função 'calculaExpressao'.
-*/
-void	 posfixaExpressao (char expressao[], int *tamanho, char saida[]);
-
-/**
-* Resolve a expressão posfixa dada, usando os espaços adicionados pela função
-* 'posfixaExpressao' para saber onde um número acaba e outro começa.
-*/
-void 	 calculaExpressao (char e_posfixa[]);
-
-
-/**
-* Empilha os números da string. Transforma números do tipo char com 
-* várias casas em um único inteiro correspondente, quando for o caso.
-*/
-void 	 empilhaNumero (t_pilha *pilha, int *i, char string[]);
-
-/**
-* Faz as operações nos elementos da pilha e coloca o resultado em seu topo.
-*/
-void	 operaNumero   (t_pilha *pilha, int *i, char string[]);
 
 int      pilhaVazia (t_pilha *pilha);
 int      pilhaCheia (t_pilha *pilha);
@@ -69,5 +31,37 @@ int      pop  (t_pilha *pilha);
 
 t_pilha* getPilha     (int tamanho);
 void     liberaPilha  (t_pilha *pilha);
+/**
+ * Recebe um caractere de fechamento de escopo e 
+ * retira elementos da pilha até encontrá-lo.
+ */
+int		 desemPilha   (t_pilha *pilha, char parentese, char saida[], int *j);
+
+/**
+ * Recebe uma expressão no formato string e valida ela quanto aos delimitadores
+ * de escopo. Retorna '1' caso seja válida e '0' caso contrário.
+ */
+int		 validaExpressao  (char expressao[], int *tamanho);
+/**
+ * Após validada, converte a espressão para a notação posfixa, adicionando
+ * espaços para facilitar a resolução dela pela função 'calculaExpressao'.
+ */
+void	 posfixaExpressao (char expressao[], int *tamanho, char saida[]);
+/**
+ * Resolve a expressão posfixa dada, usando os espaços adicionados pela função
+ * 'posfixaExpressao' para saber onde um número acaba e outro começa.
+ */
+void 	 calculaExpressao (char e_posfixa[]);
+
+
+/**
+ * Empilha os números da string. Transforma números do tipo char com 
+ * várias casas em um único inteiro correspondente, quando for o caso.
+ */
+void 	 empilhaNumero (t_pilha *pilha, int *i, char string[]);
+/**
+ * Faz as operações nos elementos da pilha e coloca o resultado em seu topo.
+ */
+void	 operaNumero   (t_pilha *pilha, int *i, char string[]);
 
 #endif
